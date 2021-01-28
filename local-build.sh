be27f72b5ba90296c6be1d7b2fa11bb90750b747
Watanabe Takashi <wtnb75@gmail.com>
2021-01-28 11:47:06 +0900

    wasm

--- local-build.sh
+++ local-build.sh
@@ -5,7 +5,7 @@ prefix=ghcr.io/wtnb75
 for i; do
   dn=${i}
   [ -f ${dn} ] && dn=$(dirname $i)
-  docker build -t ${prefix}/${dn}:latest ${dn}
+  docker build -t ${prefix}/${dn}:latest ${dn} || continue
   whalebrew uninstall -y ${prefix}/${dn}
   whalebrew install ${prefix}/${dn}
 done
